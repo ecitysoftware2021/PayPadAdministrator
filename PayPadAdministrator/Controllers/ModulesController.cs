89e0b5fab21e9850a1bec0ea96f8ec8110f66569
dcordobag <32066181+dcordobag@users.noreply.github.com>
2018-12-20 09:15:01 -0500

    Creacion del sitemap

---
+++ PayPadAdministrator/Controllers/ModulesController.cs
@@ -0,0 +1,19 @@
+﻿using PayPadAdministrator.CustomAuthentication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PayPadAdministrator.Controllers
+{
+    [CustomAuthorize]
+    public class ModulesController : Controller
+    {
+        // GET: Modules
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
