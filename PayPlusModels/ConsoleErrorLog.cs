aaa962353851b3858ec5dad099aa6a46aaab3e24
BRANDON STEVEN MONTOYA USUGA <brandonmontoya191272@correo.itm.edu.co>
2019-03-11 17:27:56 -0500

    generacion del console log

---
+++ PayPlusModels/ConsoleErrorLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PayPlusModels
+{
+    public class ConsoleErrorLog
+    {
+        public int PAYPAD_CONSOLE_ERROR_ID { get; set; }
+        public int PAYPAD_ID { get; set; }
+        public int ERROR_ID { get; set; }
+        public int ERROR_LEVEL_ID { get; set; }
+        public int? DEVICE_PAYPAD_ID { get; set; }
+        public string DESCRIPTION { get; set; }
+        public DateTime DATE { get; set; }
+        public string OBSERVATION { get; set; }
+        public bool STATE { get; set; }
+        public string PAYPAD_NAME { get; set; }
+        public string ERROR_DESCRIPTION { get; set; }
+        public int CODE_ERROR { get; set; }
+        public string ERROR_LEVEL_DESCRIPTION { get; set; }
+        public string DEVICE_NAME { get; set; }
+    }
+}
---
+++ PaypadConsoleLog/Controllers/ConsoleController.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using PaypadConsoleLog.Services;
+using PayPlusModels;
+using PayPlusModels.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PaypadConsoleLog.Controllers
+{
+    public class ConsoleController : Controller
+    {
+        static ApiService apiService = new ApiService();
+        // GET: Console
+        public async Task<ActionResult> Index(int id)
+        {
+            List<ConsoleErrorLog> consoleErrorLogs = new List<ConsoleErrorLog>();
+            var url = string.Concat(Utilities.GetConfiguration("GetConsoleLogForPayPad"), id);
+            var response = await apiService.GetDataV2(this, url);
+            if (response.CodeError == 200)
+            {
+                consoleErrorLogs = JsonConvert.DeserializeObject<List<ConsoleErrorLog>>(response.Data.ToString());
+            }
+
+            return View(consoleErrorLogs.OrderByDescending(c=>c.DATE).ToList());
+        }
+    }
+}
